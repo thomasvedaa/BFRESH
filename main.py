099ace573b6d94dffaa9c9e4ddca163065b450d8
thomasvedaa <71438936+thomasvedaa@users.noreply.github.com>
2024-04-26 11:23:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sqlite3
-
+from menu_functions import *
+from functions_database import *
+connection=sqlite3.connect('rental_database.sqlite')
 while True:
     print("1. Add/Modify car\n2. Add/Modify customer\n3. Assign car to customer\n4. Unassign car to customer"
           "\n5. Show statistics\n6. Export all information to a file\n7. Import all information from a file\n8. Exit")
     choice= int(input())
 
     if choice==1:
-        pass
+        modify_car_menu()
     if choice==2:
         pass
     if choice==3:
