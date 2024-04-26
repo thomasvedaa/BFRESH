7e6ffaa46fd737192aa125d24e5fa2dceeddcdb4
thomasvedaa <71438936+thomasvedaa@users.noreply.github.com>
2024-04-26 11:54:17 +0200

    Update menu_functions.py

--- menu_functions.py
+++ menu_functions.py
@@ -1,3 +1,7 @@
+from functions_customer import *
+from functions_car import *
+
+connection=sqlite3.connect('rental_database.sqlite')
 
 def modify_car_menu():
     while True:
@@ -9,6 +13,20 @@ def modify_car_menu():
         elif modify_car == 3:
             pass
         elif modify_car==4:
+            break
+        else:
+            print("Enter a valid number")
+
+def modify_customer_menu():
+    while True:
+        modify_customer = int(input("1. Add customer \n2. Edit customer \n3. Remove customer \n4. Exit"))
+        if modify_customer == 1:
+            add_customer_to_database(connection)
+        elif modify_customer == 2:
+            pass
+        elif modify_customer == 3:
+            pass
+        elif modify_customer==4:
             break
         else:
             print("Enter a valid number")
