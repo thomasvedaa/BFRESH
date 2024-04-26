a9f0b4eff90df8a834e04878a57aadd0de7bca75
thomasvedaa <71438936+thomasvedaa@users.noreply.github.com>
2024-04-26 10:20:10 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+while True:
+    print("1. Add/Modify car\n2. Add/Modify customer\n3. Assign car to customer\n4. Unassign car to customer"
+          "\n5. Show statistics\n6. Export all information to a file\n7. Import all information from a file\n8. Exit")
+    choice= int(input())
+
+    if choice==1:
+        pass
+    if choice==2:
+        pass
+    if choice==3:
+        pass
+    if choice==4:
+        pass
+    if choice==5:
+        pass
+    if choice==6:
+        pass
+    if choice==7:
+        pass
+    if choice==8:
+        break
