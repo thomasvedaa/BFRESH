d5e5c647f7022a05d581fd47c18c8cf9a722a78b
thomasvedaa <71438936+thomasvedaa@users.noreply.github.com>
2024-04-26 11:29:55 +0200

    Add files via upload

---
+++ functions_customer.py
@@ -0,0 +1,12 @@
+def add_customer_to_database(connection):
+    cursor = connection.cursor()
+    print("Add a customer")
+    social_security_number = input("Enter social security number: ")
+    name = input("Enter customer name: ")
+    adress = input("Enter customer adress: ")
+    telephone_number = int(input("Enter phone number: "))
+    cursor.execute('INSERT INTO Customer (Social_security_number,Name,Adress,Telephone_number) VALUES (?,?,?,?)',(social_security_number, name, adress, telephone_number))
+    connection.commit()
+    customer_id=cursor.lastrowid
+    print(f'Added customer: Id: {customer_id}, name: {name}, adress: {adress}, phone number: {telephone_number}')
+    
