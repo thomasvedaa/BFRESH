06a20700d2077c36b296757015837efe2141d85f
thomasvedaa <thomasvedaa@hotmail.no>
2024-05-03 11:47:16 +0200

    Yeet

--- functions_car.py
+++ functions_car.py
@@ -6,7 +6,7 @@ def add_car_to_database(connection):
     state=input("Enter state of the car. From 1 to 10: ")
     price=input("Enter estimated price of the car (in USD): ")
     brand=input("Enter brand: ")
-    cursor.execute('INSERT INTO Car (Registration_number,State,Price,Brand) VALUES (?,?,?,?)',(reg_nr,state,price,brand))
+    cursor.execute('INSERT INTO Car (Registration_number,State,Price_range,Brand) VALUES (?,?,?,?)',(reg_nr,state,price,brand))
     connection.commit()
     car_id=cursor.lastrowid
     print(f'Added car: Id: {car_id}, registration number: {reg_nr}, price: {price}, brand: {brand}')
