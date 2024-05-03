e4950105cd80d6c931cbb3088b619006d0dcb067
thomasvedaa <71438936+thomasvedaa@users.noreply.github.com>
2024-05-03 13:04:12 +0200

    Update functions_car.py

--- functions_car.py
+++ functions_car.py
@@ -13,7 +13,25 @@ def add_car_to_database(connection):
 
 
 def edit_car():
-    pass
+    cursor = connection.cursor()
+    car_id = int(input("Give a valid Car ID to search after: "))
+    cursor.execute('SELECT * FROM Car WHERE Car_ID = ?', (ID,))
+    car = cursor.fetchone()
+    if car is None:
+        print("No car with that ID")
+        return
+    else:
+        print(f'Found car: Id: {car[0]}, state of car: {car[2]}, registration number: {car[1]}, price: {car[3]}, brand: {car[4]}')
+        reg_nr = input("Update registration number: ")
+        state = input("Update state of the car. From 1 to 10: ")
+        price = input("Update estimated price of the car (in USD): ")
+        brand = input("Update brand: ")
+        cursor.execute('UPDATE Car SET Registration_number = ?, State = ?, Price = ?, Brand = ?',
+                       (reg_nr, state, price, brand))
+        connection.commit()
+        car_id = ID
+        print(f'Updated car: Id: {car_id},state of car: {state} registration number: {reg_nr}, price: {price}, brand: {brand}')
+
 
 def remove_car():
     pass
