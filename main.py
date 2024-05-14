c080a9e5c8fcdb8dab98d920bf6899f11e04c7f7
thomasvedaa <thomasvedaa@hotmail.no>
2024-05-14 13:35:40 +0200

    l

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-import sqlite3
 from menu_functions import *
 
-while True:
-    print("1. Add/Modify car\n2. Add/Modify customer\n3. Assign car to customer\n4. Show statistics\n5. Export all information to a file\n"
-          "5. Import all information from a file\n7. Exit")
-    choice= int(input())
-
-    if choice==1:
-        car_menu.modify_car_menu()
-    if choice==2:
-        customer_menu.modify_customer_menu()
-    if choice==3:
-        assign_car.assign_menu()
-    if choice==4:
-        stats.show_stats_menu()
-    if choice==5:
-        pass
-    if choice==6:
-        pass
-    if choice==7:
-        break
-
-
+main.menu()
