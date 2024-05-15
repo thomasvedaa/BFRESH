c82eab91c02688340c368cd40892c8e40264b958
thomasvedaa <thomasvedaa@hotmail.no>
2024-05-15 13:32:47 +0200

    Snart ferdig med funksjon

--- assign_function.py
+++ assign_function.py
@@ -6,84 +6,37 @@ class Assign_car:
         self.connection = sqlite3.connect(db)
         self.cursor = self.connection.cursor()
     def assign_car_to_customer(self):
-        customer_ssn=int(input('Enter social security number to identify yourself: '))
-        self.cursor.execute('SELECT * FROM Customer WHERE Social_security_number=?', (customer_ssn,))
-        customer=self.cursor.fetchone()
-        customer_id=customer[0]
-        if customer is None:
-            print("No customer with that social security number")
+        customer_name_choice=input('Enter the name of the customer you want')
+        self.cursor.execute('SELECT * FROM Customer WHERE Name LIKE ?', ('%'+customer_name_choice+'%',))
+        customers=self.cursor.fetchall()
+        if not customers:
+            print("No customers")
+            return
 
-        else:
+        for customer in customers:
             print(f'Found customer: Id: {customer[0]}, social security number: {customer[1]}, name: {customer[2]}, '
                   f'address: {customer[3]}, telephone number: {customer[4]}\n')
-            print('Select a car: ')
-            self.cursor.execute('SELECT * FROM Car')
-            cars=self.cursor.fetchall()
-            if not cars:
-                print('No cars available')
-            for car in cars:
-                print(f'Car: Id: {car[0]}, registration number: {car[1]}, state of car: {car[2]},  price: ${car[3]}, brand: {car[4]}')
 
+        customer_choice = int(input('Enter ID of the customer you want: '))
+        self.cursor.execute('Select * FROM Customer WHERE Customer_ID=?',(customer_choice))
+        chosen_customer=self.cursor.fetchone()
+        customer_id=chosen_customer[0]
 
-        while True:
-            car_choice=int(input("Which car do you want?\n"))
-            Start_date=input('Enter start date: ')
-            End_date=input('Enter end date: ')
-            self.cursor.execute('SELECT * FROM Car')
-            car=self.cursor.fetchone()
-            if car_choice > car[0]:
-                print("Enter a valid ID\n")
-            else:
-                start_date=input('Enter start date of rental in DD.MM.YYYY')
-                end_date=input('Enter end date of rental in DD.MM.YYYY')
-                self.cursor.execute('INSERT INTO Active_rental (Car_ID,Customer_ID,Start_date, End_date) VALUES (?,?,?,?)',(car_choice,customer_id,start_date,end_date))
-                self.connection.commit()
-                print('Car successfully assigned')
-                break
+        car_choice=input('Enter the brand of the car you want')
+        self.cursor.execute('SELECT * FROM Car WHERE Brand LIKE ?',('%'+car_choice+'%',))
+        cars=self.cursor.fetchall()
+        if not cars:
+            print('No cars')
+            return
 
-
-
-
+        for car in cars:
+            print(
+                f'Car: Id: {car[0]}, registration number: {car[1]}, state of car: {car[2]},  price: ${car[3]}, brand: {car[4]}')
 
 
 
     def unassign_car_to_customer(self):
-        customer_ssn = int(input('Enter social security number to identify yourself: '))
-        self.cursor.execute('SELECT * FROM Customer WHERE Customer_ID=? VALUES(?)',(customer_ssn))
-        customer = self.cursor.fetchone()
-        customer_id = customer[0]
-        self.cursor.execute('SELECT * FROM Active_rental,Customer WHERE Active_rental.Customer_ID=Customer.Customer_ID AND Customer.Social_security_number=?', (customer_ssn,))
-        customer_rentals = self.cursor.fetchall()
-
-
-        if customer is None:
-            print("No customer with that social security number")
-        else:
-            print(f'Found customer: Id: {customer[0]}, social security number: {customer[1]}, name: {customer[2]}, '
-                  f'address: {customer[3]}, telephone number: {customer[4]}\n')
-            print(f'Car: Id: {car[0]}, registration number: {car[1]}, state of car: {car[2]},  price: ${car[3]}, brand: {car[4]}')
-            print('Select a car: ')
-            self.cursor.execute('SELECT * FROM Car WHERE car[0]')
-            cars = self.cursor.fetchall()
-
-        if customer_rentals is None:
-            print('No rentals for this customer')
-        else:
-            print(f'Rentals found:\n Car ID: {customer_rentals[1]}, Customer ID: {customer_rentals[2]}, start date: {customer_rentals[3]}, end date: {customer_rentals[4]}')
-        while True:
-            car_choice = int(input("Which car do you want to unassign?\n"))
-            if car_choice > car[0]:
-                print("Enter a valid ID\n")
-            else:
-                self.cursor.execute('DELETE FROM Active_rental WHERE Car_ID=? AND Customer_ID=?', (car_choice, customer_id))
-                self.connection.commit()
-                print('Car successfully unassigned')
-                break
-
-
-
-
-
+        pass
     def assign_menu(self):
         while True:
             assign_choice = int(input("1. Assign car to customer\n2. Unassign car to customer \n3. Exit\n"))
