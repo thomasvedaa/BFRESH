59fdd1334c9530a8c0dd401fd30c29f60466cfc3
thomasvedaa <thomasvedaa@hotmail.no>
2024-05-21 14:31:06 +0200

    m

--- functions_stats.py
+++ functions_stats.py
@@ -9,18 +9,18 @@ class Stats:
         self.cursor = self.connection.cursor()
     def count_customers(self):
         self.cursor.execute('SELECT COUNT(Customer_ID) FROM Customer')
-        customer_count=self.cursor.fetchall()
-        print(f'There are {customer_count[0][0]} customers')
+        customer_count=self.cursor.fetchone()
+        print(f'There are {customer_count[0]} customers')
 
     def count_cars(self):
         self.cursor.execute('SELECT COUNT(Car_ID) FROM Car')
-        car_count=self.cursor.fetchall()
-        print(f'There are {car_count[0][0]} cars')
+        car_count=self.cursor.fetchone()
+        print(f'There are {car_count[0]} cars')
 
     def count_active_rentals(self):
         self.cursor.execute('SELECT COUNT(Rental_ID) FROM Active_rental')
-        rental_count=self.cursor.fetchall()
-        print(f'There are {rental_count[0][0]} active rentals')
+        rental_count=self.cursor.fetchone()
+        print(f'There are {rental_count[0]} active rentals')
 
     def count_completed_rentals(self):
         current_date = datetime.now().strftime('%Y-%m-%d')
@@ -30,7 +30,8 @@ class Stats:
     def show_stats_menu(self):
         while True:
             stats_choice = int(input(
-                "1. Number of customers \n2. Number of cars \n3. Number of active rentals \n4. Number of completed rentals \n5. Exit \n"))
+                "1. Number of customers \n2. Number of cars \n"
+                "3. Number of active rentals \n4. Number of completed rentals \n5. Exit \n"))
             if stats_choice == 1:
                 self.count_customers()
             elif stats_choice == 2:
